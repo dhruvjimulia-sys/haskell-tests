4789b3517d44bd044309490f18d94d82b053d0bf
Dhruv Jimulia <dj321@ic.ac.uk>
2021-12-30 01:21:58 +0530

    Completed 2013-binomialheaps

--- 2013-binomialheaps/template.hs
+++ 2013-binomialheaps/template.hs
@@ -1,3 +1,4 @@
+import Data.Maybe
 type BinHeap a = [BinTree a]
 
 data BinTree a = Node a Int (BinHeap a)
@@ -7,62 +8,87 @@ data BinTree a = Node a Int (BinHeap a)
 -- PART I
 
 key :: BinTree a -> a
-key
-  = undefined
+key (Node k _ _)
+  = k
 
 rank :: BinTree a -> Int
-rank
-  = undefined
+rank (Node _ r _)
+  = r
 
 children :: BinTree a -> [BinTree a]
-children
-  = undefined
+children (Node _ _ cs)
+  = cs
 
 combineTrees :: Ord a => BinTree a -> BinTree a -> BinTree a
-combineTrees 
-  = undefined
+combineTrees t@(Node k r cs) t'@(Node k' _ cs')
+  | k < k'    = Node k (r + 1) (t':cs)
+  | otherwise = Node k' (r + 1) (t:cs')
 
 --------------------------------------------------------------
 -- PART II
 
 extractMin :: Ord a => BinHeap a -> a
-extractMin 
-  = undefined
+extractMin h
+  = minimum $ map key h
 
 mergeHeaps :: Ord a => BinHeap a -> BinHeap a -> BinHeap a
-mergeHeaps 
-  = undefined
+mergeHeaps [] h
+  = h
+mergeHeaps h []
+  = h
+mergeHeaps h@(t@(Node _ r _):ts) h'@(t'@(Node _ r' _):ts')
+  | r < r'    = t : mergeHeaps ts h'
+  | r' < r    = t' : mergeHeaps ts' h
+  | otherwise = mergeHeaps [combineTrees t t'] (mergeHeaps ts ts')
 
 insert :: Ord a => a -> BinHeap a -> BinHeap a
-insert 
-  = undefined
+insert v
+  = mergeHeaps [Node v 0 []]
 
 deleteMin :: Ord a => BinHeap a -> BinHeap a
-deleteMin 
-  = undefined
-
-remove :: Eq a => a -> BinHeap a -> BinHeap a
-remove
-  = undefined
-
-removeMin :: Ord a => BinHeap a -> (BinTree a, BinHeap a)
-removeMin
-  = undefined
+deleteMin h
+  = mergeHeaps (filter (/= t) h) h'
+  where m = extractMin h
+        t = head [t | t@(Node k _ _) <- h, k == m]
+        h' = (reverse . children) t
 
 binSort :: Ord a => [a] -> [a]
-binSort 
-  = undefined
+binSort xs
+  = flatten (foldr insert [] xs)
+  where flatten :: Ord a => BinHeap a -> [a]
+        flatten [] = []
+        flatten h  = extractMin h : flatten (deleteMin h) 
 
 --------------------------------------------------------------
 -- PART III
 
 toBinary :: BinHeap a -> [Int]
-toBinary
-  = undefined
+toBinary xs
+  = reverse $ toBinary' $ map (\(Node _ r _) -> r) xs
+  where toBinary' :: [Int] -> [Int]
+        toBinary' xs
+          | all (<0) xs = []
+          | 0 `elem` xs = 1 : toBinary' (map (\x -> x - 1) xs)
+          | otherwise   = 0 : toBinary' (map (\x -> x - 1) xs)
 
 binarySum :: [Int] -> [Int] -> [Int]
-binarySum
-  = undefined
+binarySum i i'
+  = reverse $ binarySum' (reverse i) (reverse i') 0
+  where binarySum' :: [Int] -> [Int] -> Int -> [Int]
+        binarySum' (x:xs) (y:ys) ci
+          = s : binarySum' xs ys co
+          where (co, s) = quotRem (x + y + ci) 2
+        binarySum' (x:xs) [] ci
+          = s : binarySum' xs [] co
+          where (co, s) = quotRem (x + ci) 2
+        binarySum' [] (y:ys) ci
+          = s : binarySum' [] ys co
+          where (co, s) = quotRem (y + ci) 2
+        binarySum' _ _ 1
+          = [1]
+        binarySum' _ _ _
+          = [] 
+        
 
 ------------------------------------------------------
 -- Some sample trees...
@@ -73,20 +99,20 @@ t1, t2, t3, t4, t5, t6, t7, t8 :: BinTree Int
 -- t1 to t4 appear in Figure 1...
 t1 = Node 4 0 []
 t2 = Node 1 1 [Node 5 0 []]
-t3 = Node 2 2 [Node 8 1 [Node 9 0 []], 
+t3 = Node 2 2 [Node 8 1 [Node 9 0 []],
                Node 7 0 []]
-t4 = Node 2 3 [Node 3 2 [Node 6 1 [Node 8 0 []], 
+t4 = Node 2 3 [Node 3 2 [Node 6 1 [Node 8 0 []],
                          Node 10 0 []],
                Node 8 1 [Node 9 0 []],
                Node 7 0 []]
 
 -- t5 and t6 are on the left of Figure 2; t7 is on the
 -- right
-t5 = Node 4 2 [Node 6 1 [Node 8 0 []], 
+t5 = Node 4 2 [Node 6 1 [Node 8 0 []],
                          Node 10 0 []]
 t6 = Node 2 2 [Node 8 1 [Node 9 0 []], Node 7 0 []]
 t7 = Node 2 3 [Node 4 2 [Node 6 1 [Node 8 0 []], Node 10 0 []],
-               Node 8 1 [Node 9 0 []], 
+               Node 8 1 [Node 9 0 []],
                Node 7 0 []]
 
 -- An additional tree...
