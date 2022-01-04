93475fff7604b9a253ca2817b104b0d74959fe00
Dhruv Jimulia <dj321@ic.ac.uk>
2022-01-04 19:35:40 +0530

    Completed till Part 3 of 2018-constpropogation

--- 2018-constpropogation/templates.hs
+++ 2018-constpropogation/templates.hs
@@ -1,5 +1,6 @@
 import Data.Maybe
 import Data.List
+import Debug.Trace
 
 type Id = String
 
@@ -9,13 +10,13 @@ type Block = [Statement]
 
 data Statement = Assign Id Exp |
                  If Exp Block Block |
-                 DoWhile Block Exp 
+                 DoWhile Block Exp
                deriving (Eq, Show)
 
 data Exp = Const Int | Var Id | Apply Op Exp Exp | Phi Exp Exp
          deriving (Eq, Show)
 
-data Op = Add | Mul | Eq | Gtr 
+data Op = Add | Mul | Eq | Gtr
         deriving (Eq, Show)
 
 ------------------------------------------------------------------------
@@ -23,8 +24,8 @@ data Op = Add | Mul | Eq | Gtr
 
 lookUp :: (Eq a, Show a) => a -> [(a, b)] -> b
 lookUp i table
-  = fromMaybe (error ("lookup failed on identifier: " ++ show i)) 
-              (lookup i table) 
+  = fromMaybe (error ("lookup failed on identifier: " ++ show i))
+              (lookup i table)
 
 execFun :: Function -> [Int] -> State
 execFun (name, args, p) vs
@@ -36,26 +37,45 @@ execFun (name, args, p) vs
 type State = [(Id, Int)]
 
 update :: (Id, Int) -> State -> State
-update 
-  = undefined
+update b@(k, _) s
+  = b : filter (\(k', _) -> k' /= k) s
 
 apply :: Op -> Int -> Int -> Int
-apply 
-  = undefined
+apply Add i i'
+  = i + i'
+apply Mul i i'
+  = i * i'
+apply Eq i i'
+  = fromEnum (i == i')
+apply Gtr i i'
+  = fromEnum (i > i')
 
 eval :: Exp -> State -> Int
 -- Pre: the variables in the expression will all be bound in the given state 
 -- Pre: expressions do not contain phi instructions
-eval 
-  = undefined
+eval (Const i) _
+  = i
+eval (Var v) s
+  = lookUp v s
+eval (Apply o e e') s
+  = apply o (eval e s) (eval e' s)
+
+
 
 execStatement :: Statement -> State -> State
-execStatement 
-  = undefined
+execStatement (Assign i e) s
+  = update (i, eval e s) s
+execStatement (If e b b') s
+  | eval e s == 1 = execBlock b s
+  | otherwise     = execBlock b' s
+execStatement (DoWhile b e) s
+  | eval e s' == 1 = execStatement (DoWhile b e) s'
+  | otherwise      = s'
+  where s' = execBlock b s
 
 execBlock :: Block -> State -> State
-execBlock 
-  = undefined
+execBlock bs s
+  = foldl (flip execStatement) s bs
 
 ------------------------------------------------------------------------
 -- Given function for testing propagateConstants...
@@ -70,23 +90,61 @@ applyPropagate (name, args, body)
 
 foldConst :: Exp -> Exp
 -- Pre: the expression is in SSA form
-foldConst 
-  = undefined
+foldConst (Phi (Const c) (Const c'))
+  | c == c' = Const c
+foldConst (Apply o (Const c) (Const c'))
+  = Const (apply o c c')
+foldConst (Apply Add (Const 0) (Var v))
+  = Var v
+foldConst (Apply Add (Var v) (Const 0))
+  = Var v
+foldConst e
+  = e
 
 sub :: Id -> Int -> Exp -> Exp
 -- Pre: the expression is in SSA form
-sub 
-  = undefined
+sub id i (Var id')
+  | id == id' = Const i
+  | otherwise = Var id' 
+sub _ _ (Const i)
+  = Const i
+sub id i (Apply o e e')
+  = foldConst (Apply o (sub id i e) (sub id i e'))
+sub id i (Phi e e')
+  = foldConst (Phi (sub id i e) (sub id i e'))
 
 -- Use (by uncommenting) any of the following, as you see fit...
--- type Worklist = [(Id, Int)]
+type Worklist = [(Id, Int)]
 -- scan :: Id -> Int -> Block -> (Worklist, Block)
--- scan :: (Exp -> Exp) -> Block -> (Exp -> Exp, Block)
- 
+
+
 propagateConstants :: Block -> Block
 -- Pre: the block is in SSA form
-propagateConstants 
-  = undefined
+propagateConstants b
+  = snd $ until (null . fst) (\((v, c): wl, b') -> let (wl', b'') = scan v c b' in (wl' ++ wl, b'')) (scan "$INVALID" 0 b)
+
+scan :: Id -> Int -> Block -> (Worklist, Block)
+scan id i []
+  = ([], [])
+scan id i ((Assign "$return" e): _)
+  = ([], [Assign "$return" (sub id i e)])
+scan id i ((Assign v (Const c)): ss)
+  = ((v, c) : wl, b')
+  where (wl, b') = scan id i ss
+scan id i ((Assign v e): ss)
+  = case sub id i e of
+      (Const c) -> ((v, c): wl, b)
+      e         -> (wl, Assign v (sub id i e): b)
+  where (wl, b) = scan id i ss
+scan id i ((If e b b'): ss)
+  = (wl ++ wl' ++ wl'', If (sub id i e) mb mb': mb'')
+  where (wl, mb) = scan id i b
+        (wl', mb') = scan id i b'
+        (wl'', mb'') = scan id i ss
+scan id i ((DoWhile b e): ss)
+  = (wl ++ wl' , DoWhile mb (sub id i e): mb')
+  where (wl, mb) = scan id i b
+        (wl', mb') = scan id i ss
 
 ------------------------------------------------------------------------
 -- Given functions for testing unPhi...
@@ -107,14 +165,27 @@ optimise (name, args, body)
 
 unPhi :: Block -> Block
 -- Pre: the block is in SSA form
-unPhi 
-  = undefined
+unPhi (If e b b': ss)
+  = If e (unPhi (b ++ b1)) (unPhi (b' ++ b2)): ss'
+  where (b1, b2, ss') = unPhi' ss   
+unPhi (DoWhile ss e: ss')
+ = (b1 ++ [DoWhile (unPhi (ss'' ++ b2)) e]) ++ ss'
+ where (b1, b2, ss'') = unPhi' ss
+unPhi (s:ss)
+  = s : unPhi ss
+unPhi []
+  = []
+
+unPhi' :: Block -> (Block, Block, Block)
+unPhi' (Assign v (Phi e1 e2): ss) = (Assign v e1: b1, Assign v e2: b2, ss')
+  where (b1, b2, ss') = unPhi' ss
+unPhi' ss = ([], [], ss)
 
 ------------------------------------------------------------------------
 -- Part IV
 
 makeSSA :: Function -> Function
-makeSSA 
+makeSSA
   = undefined
 
 
@@ -130,22 +201,22 @@ precTable
 prec op
   = lookUp op precTable
 
-showArgs [] 
+showArgs []
   = ""
 showArgs as
   = foldr1 (\a s -> a ++ (", " ++ s)) as
 
 showExp :: Int -> Exp -> String
-showExp _ (Const n) 
+showExp _ (Const n)
   = show n
-showExp _ (Var id) 
+showExp _ (Var id)
   = id
-showExp n (Apply op' e e') 
+showExp n (Apply op' e e')
   | n > n'    = "(" ++ s ++ ")"
   | otherwise = s
-  where 
+  where
     n' = prec op'
-    s = showExp n' e ++ " " ++ fromJust (lookup op' opNames ) ++ " " ++ 
+    s = showExp n' e ++ " " ++ fromJust (lookup op' opNames ) ++ " " ++
         showExp n' e'
 showExp _ (Phi e e')
   = "PHI(" ++ showArgs (map (showExp 0) [e, e']) ++ ")"
@@ -169,12 +240,12 @@ showBlock' b n
     showStatement (If p q []) n k
       = do showLine ("if " ++ "(" ++ showExp 0 p ++ ") {") n k
            n' <- showBlock'' q (n + 1) (k + 2)
-           showLine "}" n' k 
+           showLine "}" n' k
            return (n' + 1)
     showStatement (If p q r) n k
       = do showLine ("if " ++ "(" ++ showExp 0 p ++ ") {") n k
            n'  <- showBlock'' q (n + 1) (k + 2)
-           showLine "} else {" n' k 
+           showLine "} else {" n' k
            n'' <- showBlock'' r (n' + 1) (k + 2)
            showLine "}" n'' k
            return (n'' + 1)
@@ -213,13 +284,13 @@ e5 = Apply Add (Var "a") (Var "x")
 
 -- Figure 1...
 example :: Function
-example 
+example
   = ("example",["x"],[Assign "a" (Const 1),Assign "b" (Apply Add (Var "x")
-    (Const 2)),Assign "c" (Const 3),If (Apply Eq (Var "x") (Const 10)) 
-    [Assign "a" (Const 1),Assign "c" (Const 5)] [],Assign "d" 
+    (Const 2)),Assign "c" (Const 3),If (Apply Eq (Var "x") (Const 10))
+    [Assign "a" (Const 1),Assign "c" (Const 5)] [],Assign "d"
     (Apply Add (Var "a") (Const 3)),Assign "e" (Apply Add (Var "d") (Var "b")),
     Assign "$return" (Apply Add (Var "e") (Var "c"))])
-    
+
 exampleSSA :: Function
 exampleSSA
   = ("example",["x"],[Assign "a0" (Const 1),Assign "b0" (Apply Add (Var "x")
@@ -228,35 +299,35 @@ exampleSSA
     "a0")),Assign "c2" (Phi (Var "c1") (Var "c0")),Assign "d0" (Apply Add (Var
     "a2") (Const 3)),Assign "e0" (Apply Add (Var "d0") (Var "b0")),
     Assign "$return" (Apply Add (Var "e0") (Var "c2"))])
-    
+
 exampleSSAPropagated :: Function
 exampleSSAPropagated
   = ("example",["x"],[Assign "b0" (Apply Add (Var "x") (Const 2)),If (Apply Eq
     (Var "x") (Const 10)) [] [],Assign "c2" (Phi (Const 5) (Const 3)),
-    Assign "e0" (Apply Add (Const 4) (Var "b0")),Assign "$return" 
+    Assign "e0" (Apply Add (Const 4) (Var "b0")),Assign "$return"
     (Apply Add (Var "e0") (Var "c2"))])
 
 exampleOptimised :: Function
-exampleOptimised 
+exampleOptimised
   = ("example",["x"],[Assign "b0" (Apply Add (Var "x") (Const 2)),If (Apply Eq
     (Var "x") (Const 10)) [Assign "c2" (Const 5)] [Assign "c2" (Const 3)],Assign
     "e0" (Apply Add (Const 4) (Var "b0")),Assign "$return" (Apply Add (Var "e0")
     (Var "c2"))])
-    
+
 
 -- Figure 2 (there is no SSA version of this)...
 fact :: Function
-fact 
-  = ("fact", 
-     ["n"], 
+fact
+  = ("fact",
+     ["n"],
      [If (Apply Eq (Var "n") (Const 0))
         [Assign "$return" (Const 1)]
         [Assign "prod" (Const 1),
          Assign "i" (Var "n"),
-         DoWhile 
+         DoWhile
            [Assign "prod" (Apply Mul (Var "prod") (Var "i")),
             Assign "i" (Apply Add (Var "i") (Const (-1)))
-           ] 
+           ]
            (Apply Gtr (Var "i") (Const 0)),
          Assign "$return" (Var "prod")
         ]
@@ -266,62 +337,62 @@ fact
 
 -- Summation loop, specialised loop for the case k=0...
 loop :: Function
-loop 
+loop
   = ("loop",["n"],[Assign "i" (Var "n"),Assign "k" (Const 0),Assign "sum"
     (Const 0),If (Apply Eq (Var "i") (Const 0)) [Assign "$return" (Const 0)]
-    [DoWhile [Assign "sum" (Apply Add (Var "sum") (Apply Mul (Apply Add 
-    (Var "i") (Apply Mul (Const 2) (Var "k"))) (Apply Add (Apply Add (Var "i") 
-    (Apply Mul (Const 2) (Var "k"))) (Const 1)))),Assign "i" (Apply Add 
+    [DoWhile [Assign "sum" (Apply Add (Var "sum") (Apply Mul (Apply Add
+    (Var "i") (Apply Mul (Const 2) (Var "k"))) (Apply Add (Apply Add (Var "i")
+    (Apply Mul (Const 2) (Var "k"))) (Const 1)))),Assign "i" (Apply Add
     (Var "i") (Const (-1)))] (Apply Gtr (Var "i") (Const 0)),
     Assign "$return" (Var "sum")]])
-    
+
 loopSSA :: Function
 loopSSA
   = ("loop",["n"],[Assign "i0" (Var "n"),Assign "k0" (Const 0),Assign "sum0"
     (Const 0),If (Apply Eq (Var "i0") (Const 0)) [Assign "$return" (Const 0)]
-    [DoWhile [Assign "sum1" (Phi (Var "sum0") (Var "sum2")),Assign "i1" 
+    [DoWhile [Assign "sum1" (Phi (Var "sum0") (Var "sum2")),Assign "i1"
     (Phi (Var "i0") (Var "i2")),Assign "k1" (Apply Mul (Var "k0") (Const 2)),
-    Assign "a0" (Apply Add (Var "i1") (Var "k1")),Assign "k2" (Apply Mul 
+    Assign "a0" (Apply Add (Var "i1") (Var "k1")),Assign "k2" (Apply Mul
     (Var "k0") (Const 2)),Assign "b0" (Apply Add (Var "k2") (Const 1)),
-    Assign "b1" (Apply Add (Var "i1") (Var "b0")),Assign "m0" (Apply Mul 
+    Assign "b1" (Apply Add (Var "i1") (Var "b0")),Assign "m0" (Apply Mul
     (Var "a0") (Var "b1")),Assign "sum2" (Apply Add (Var "sum1") (Var "m0")),
-    Assign "i2" (Apply Add (Var "i1") (Const (-1)))] (Apply Gtr (Var "i2") 
+    Assign "i2" (Apply Add (Var "i1") (Const (-1)))] (Apply Gtr (Var "i2")
     (Const 0)),Assign "$return" (Var "sum2")]])
-    
+
 loopSSAPropagated :: Function
-loopSSAPropagated 
+loopSSAPropagated
   = ("loop",["n"],[Assign "i0" (Var "n"),If (Apply Eq (Var "i0") (Const 0))
     [Assign "$return" (Const 0)] [DoWhile [Assign "sum1" (Phi (Const 0) (Var
     "sum2")),Assign "i1" (Phi (Var "i0") (Var "i2")),Assign "a0" (Var "i1"),
-    Assign "b1" (Apply Add (Var "i1") (Const 1)),Assign "m0" (Apply Mul 
+    Assign "b1" (Apply Add (Var "i1") (Const 1)),Assign "m0" (Apply Mul
     (Var "a0") (Var "b1")),Assign "sum2" (Apply Add (Var "sum1") (Var "m0")),
-    Assign "i2" (Apply Add (Var "i1") (Const (-1)))] (Apply Gtr (Var "i2") 
+    Assign "i2" (Apply Add (Var "i1") (Const (-1)))] (Apply Gtr (Var "i2")
     (Const 0)),Assign "$return" (Var "sum2")]])
- 
+
 loopOptimised :: Function
-loopOptimised 
+loopOptimised
   = ("loop",["n"],[Assign "i0" (Var "n"),If (Apply Eq (Var "i0") (Const 0))
     [Assign "$return" (Const 0)] [Assign "sum1" (Const 0),Assign "i1" (Var
-    "i0"),DoWhile [Assign "a0" (Var "i1"),Assign "b1" (Apply Add (Var "i1") 
-    (Const 1)),Assign "m0" (Apply Mul (Var "a0") (Var "b1")),Assign "sum2" 
-    (Apply Add (Var "sum1") (Var "m0")),Assign "i2" (Apply Add (Var "i1") 
-    (Const (-1))),Assign "sum1" (Var "sum2"),Assign "i1" (Var "i2")] 
+    "i0"),DoWhile [Assign "a0" (Var "i1"),Assign "b1" (Apply Add (Var "i1")
+    (Const 1)),Assign "m0" (Apply Mul (Var "a0") (Var "b1")),Assign "sum2"
+    (Apply Add (Var "sum1") (Var "m0")),Assign "i2" (Apply Add (Var "i1")
+    (Const (-1))),Assign "sum1" (Var "sum2"),Assign "i1" (Var "i2")]
     (Apply Gtr (Var "i2") (Const 0)),Assign "$return" (Var "sum2")]])
-    
+
 
 -- Basic block (no conditionals or loops)...
 basicBlock :: Function
-basicBlock 
+basicBlock
   = ("basicBlock",[],[Assign "x" (Const 1),Assign "y" (Const 2),Assign "x"
     (Apply Add (Var "x") (Var "y")),Assign "y" (Apply Mul (Var "x") (Const
     3)),Assign "$return" (Var "y")])
-    
+
 basicBlockSSA :: Function
-basicBlockSSA 
+basicBlockSSA
   = ("basicBlock",[],[Assign "x0" (Const 1),Assign "y0" (Const 2),Assign "x1"
     (Apply Add (Var "x0") (Var "y0")),Assign "y1" (Apply Mul (Var "x1") (Const
     3)),Assign "$return" (Var "y1")])
-    
+
 basicBlockSSAPropagated :: Function
 basicBlockSSAPropagated
   = ("basicBlock", [], [Assign "$return" (Const 9)])
@@ -333,12 +404,12 @@ basicBlockOptimised
 
 -- Computes the maximum of two integers; useful for testing unPhi...
 max2 :: Function
-max2 
+max2
   = ("max2",["x","y"],[If (Apply Gtr (Var "x") (Var "y")) [Assign "m" (Var "x")]
     [Assign "m" (Var "y")],Assign "$return" (Var "m")])
 
 max2SSA :: Function
-max2SSA 
+max2SSA
   = ("max2",["x","y"],[If (Apply Gtr (Var "x") (Var "y")) [Assign "m0" (Var
     "x")] [Assign "m1" (Var "y")],Assign "m2" (Phi (Var "m0") (Var "m1")),Assign
     "$return" (Var "m2")])
@@ -350,7 +421,7 @@ max2SSAPropagated
     "$return" (Var "m2")])
 
 max2Optimised :: Function
-max2Optimised 
+max2Optimised
   = ("max2",["x","y"],[If (Apply Gtr (Var "x") (Var "y")) [Assign "m0" (Var
     "x"),Assign "m2" (Var "m0")] [Assign "m1" (Var "y"),Assign "m2" (Var
     "m1")],Assign "$return" (Var "m2")])
